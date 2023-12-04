e8df3aca2e5a910c6a90257a83c413362714d355
Darya <d9041261928@gmail.com>
2023-12-04 23:27:46 +0200

    add 3,4,5 labs

---
+++ OOP_3/OOP_3.1.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include "Header.h"
+
+int main() {
+
+	AS::Array3d<int> array3D(3, 3, 3);
+	array3D.fill_1(2);
+	array3D.Print();
+
+
+	array3D.setValue(2, 3, 1, 999);
+	std::cout << array3D.getValue(1, 1, 1) << std::endl;
+
+	array3D.zeroes();
+	array3D.Print();
+	return 0;
+
+
+
+}
