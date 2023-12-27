cf4c0038266bc6aab9fda60329d9f6f7e99d1fb9
Darya <d9041261928@gmail.com>
2023-12-27 20:16:37 +0200

    Lab 4

---
+++ OOP_4/Workflow.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Keyboard.h"
+
+class Workflow
+{
+public:
+    explicit Workflow(Keyboard& keyboard) : keyboard(keyboard) {}
+    
+    void keypress(const std::string& key);
+
+    void undo();
+    void perform();
+
+    void addAction(const Action& action);
+
+private:
+    Keyboard& keyboard;
+    std::vector<Action> actions; // вектор действий для выполнения в рабочем процессе (workflow)
+};
